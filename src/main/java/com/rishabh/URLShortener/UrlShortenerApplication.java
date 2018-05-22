839ba29c044ee1c29c06b624f315cee7e2bac5da
Rishabh Sharma <rsharma@qca-sdc.com>
2018-05-22 13:11:13 -0500

    Project Initialise

---
+++ src/main/java/com/rishabh/URLShortener/UrlShortenerApplication.java
@@ -0,0 +1,19 @@
+package com.rishabh.URLShortener;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerApplication.class, args);
+	}
+
+
+
+
+}
